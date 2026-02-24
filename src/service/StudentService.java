f984c0ec9250f8619c1f2742946bd56d1d0f3431
Akylbek Zamirov <124916251+akylbek04@users.noreply.github.com>
2026-02-24 12:39:33 +0600

    done

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -9,22 +9,41 @@ public class StudentService {
     private ArrayList<Student> students = new ArrayList<>();
 
     public void initializeStudents() {
-        // TODO: Add at least 5 students
+        students.add(new Student(1, "Alice", 3.8));
+        students.add(new Student(2, "Bob", 1.9));
+        students.add(new Student(3, "Charlie", 3.5));
+        students.add(new Student(4, "Diana", 1.7));
+        students.add(new Student(5, "Eve", 3.9));
     }
 
     public void removeLowGPA() {
-        // TODO: Remove students with GPA < 2.0
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().getGpa() < 2.0) {
+                iterator.remove();
+            }
+        }
     }
 
     public void findHighestGPA() {
-        // TODO: Find and print student with highest GPA
+        Student best = students.get(0);
+        for (Student s : students) {
+            if (s.getGpa() > best.getGpa()) {
+                best = s;
+            }
+        }
+        System.out.println("Highest GPA: " + best);
     }
 
     public void insertAtIndex() {
-        // TODO: Insert new student at index 2
+        int id = students.size() + 1;
+        students.add(2, new Student(id, "New Student", 3.2));
     }
 
     public void printStudents() {
-        // TODO: Print using Iterator
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
